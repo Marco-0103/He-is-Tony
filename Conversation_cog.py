2dbb86b08f18c9ad83b99dd2f1ebd6fbb7096ffd
Marco-0103 <marcolai0103@gmail.com>
2024-12-20 04:03:04 +0800

    Update main.py and other cogs

---
+++ Conversation_cog.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+
+async def setup(bot):
+    bot.add_command(sleep)
+    bot.tree.add_command(intro)
+    bot.tree.add_command(hello)
+    bot.tree.add_command(smile)
+    bot.tree.add_command(gn)
+
+@commands.command()
+async def sleep(ctx):
+    await ctx.send("mua~ gn :heart: ")
+
+# Slash command
+@discord.app_commands.command(name="intro")
+async def intro(interaction: discord.Interaction ):
+    await interaction.response.send_message("My name is Huang Jun Jing Tony(Marco''s son)")
+
+@discord.app_commands.command(name="hello")
+async def hello(interaction: discord.Interaction):
+    await interaction.response.send_message("大家好我係黃俊景")
+
+@discord.app_commands.command(name="smile")
+async def smile(interaction: discord.Interaction ):
+    await interaction.response.send_message("黃俊景哈哈哈")
+
+@discord.app_commands.command(name="gn")
+async def gn(interaction: discord.Interaction ):
+    await interaction.response.send_message("gn b:heart:")
+
+#@bot.tree.command(name="love")
+#async def love(interaction: discord.Interaction ):
+#   await interaction.response.send_message("2018-2023: O2\nNow: 他是")
---
+++ Main.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Create intents
+intents = discord.Intents.default()
+intents.messages = True  # Enable message intents
+intents.guilds = True    # Enable guild intents
+intents.message_content = True  # Enable message content intent
+
+# Create bot instance
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.event
+async def on_ready():
+    await bot.tree.sync()  # Sync the application commands (slash commands) with Discord
+    print(f'Logged in as {bot.user.name}')
+
+async def load_extensions():
+    await bot.load_extension("music_cog")
+    await bot.load_extension("Conversation")
+
+@bot.event
+async def setup_hook():
+    await load_extensions()
+
+@bot.event
+async def on_disconnect():
+    print("Bot has been disconnected.")
+
+bot.run(os.getenv("DISCORD_TOKEN"))
